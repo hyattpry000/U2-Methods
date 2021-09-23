5b0a977e53b94eedc8418f8586385c66089f8633
hyattpry000 <hyattpry000@warren.k12.in.us>
2021-09-23 12:12:34 -0400

    P3PROB

--- L4-Strings/src/SplitPs.java
+++ L4-Strings/src/SplitPs.java
@@ -1,3 +1,5 @@
+import javax.swing.*;
+
 public class SplitPs {
 
     /*
@@ -5,4 +7,19 @@ public class SplitPs {
 
     "Hippopotamus" -> ppop
      */
+    public static void main(String[] args) {
+        String word = JOptionPane.showInputDialog("Enter your word: ");
+        JOptionPane.showMessageDialog(null, splitP(word));
+        System.exit(0);
+    }
+
+
+
+    public static String splitP(String word){
+        int pIndex = word.indexOf('p');
+
+        String split = word.substring(pIndex, pIndex + 4);
+        return split;
+    }
+
 }

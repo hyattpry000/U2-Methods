3b84e3627fe0a96bd776842f40b094c618608c14
hyattpry000 <hyattpry000@warren.k12.in.us>
2021-10-22 11:47:50 -0400

    P3PROB

--- L7-Swing Colors and Paint/src/ChangingColorsGUI.java
+++ L7-Swing Colors and Paint/src/ChangingColorsGUI.java
@@ -1,14 +1,20 @@
+import javax.print.CancelablePrintJob;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class ChangingColorsGUI {
 
     //Variables in a class are declared here,
     //but often not initialized
 
-    JFrame window;
-    JPanel panel;
-    JButton colorClicker;
+    JFrame window = new JFrame("Color Switcheroo");
+    JPanel panel = new JPanel();
+    JButton darkTheme = new JButton("Dark");
+    JButton lightTheme = new JButton("Second Color");
+    JButton retroTheme = new JButton("Third Color");
+
 
     /*
     This is a constructor and absolutely necessary for a class.
@@ -16,21 +22,25 @@ public class ChangingColorsGUI {
     name as the class.
     */
 
-    public ChangingColorsGUI(){
+    public ChangingColorsGUI() {
 
         window = new JFrame("Color Changer");
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setSize(340, 127);
+        window.setSize(400, 300);
         window.setLocationRelativeTo(null);
 
         panel = new JPanel();
-        colorClicker = new JButton("Change the Colors");
+        panel.add(darkTheme);
+        panel.add(lightTheme);
+        panel.add(retroTheme);
 
         //create a new Custom color
 
         //Set the colors of our buttons and panel
+        darkTheme.addActionListener(new Dark());
+        lightTheme.addActionListener(new Light());
+        retroTheme.addActionListener(new Retro());
 
-        panel.add(colorClicker);
         window.add(panel);
 
         window.setVisible(true);
@@ -38,7 +48,43 @@ public class ChangingColorsGUI {
 
     //Add a listener to change the color when the button is clicked
 
+    private class Dark implements ActionListener {
 
+        @Override
+        public void actionPerformed(ActionEvent actionEvent) {
+            Color darkPanel = new Color(200,75,49);
+            Color darkButton = new Color(52, 103,81);
+            Color darkText = new Color(236,219,186);
 
+            darkTheme.setBackground(darkButton);
+            darkTheme.setForeground(darkText);
+            panel.setBackground(darkPanel);
+        }
+    }
+    private class Light implements ActionListener {
 
+        @Override
+        public void actionPerformed(ActionEvent actionEvent) {
+            Color lightPanel = new Color(202,184,255);
+            Color lightButton = new Color(252, 255,166);
+            Color lightText = new Color(181,222,255);
+
+            lightTheme.setBackground(lightButton);
+            lightTheme.setForeground(lightText);
+            panel.setBackground(lightPanel);
+        }
+    }
+    private class Retro implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent actionEvent) {
+            Color retroPanel = new Color(162,210,255);
+            Color retroButton = new Color(255, 134,94);
+            Color retroText = new Color(254,249,239);
+
+            retroTheme.setBackground(retroButton);
+            retroTheme.setForeground(retroText);
+            panel.setBackground(retroPanel);
+        }
+    }
 }

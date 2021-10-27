173e4eeb0370905d685d996bd04cde42fc05235c
hyattpry000 <hyattpry000@warren.k12.in.us>
2021-10-27 12:25:39 -0400

    {f}art

--- L7-Swing Colors and Paint/src/ArtworkGUI.java
+++ L7-Swing Colors and Paint/src/ArtworkGUI.java
@@ -1,20 +1,61 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class ArtworkGUI {
 
     //DECLARE VARIABLES
+    JFrame frame;
+    Color one = new Color(240,152,240);
 
     public ArtworkGUI()
     {
+        frame = new JFrame("hell yeah:)");
+        frame.setSize(600,600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+
+
+
         //CREATE THE GUI
+        JPanel panel = new ArtworkGUI.MyDrawing();
+        frame.add(panel);
+        frame.setVisible(true);
+
+
+
     }
 
     private class MyDrawing extends JPanel
     {
         public MyDrawing(){
             //Set the background color
+            setBackground(Color.black);
         }
 
         //Add the PaintComponent Method and create your drawing.
+
+        public void paintComponent(Graphics g){
+
+            super.paintComponent(g);
+
+            g.setColor(Color.yellow);
+            g.fillOval(90,75,400,400);
+
+            g.setColor(Color.black);
+            g.fillOval(150,150,100,100);
+            g.fillOval(350,150,100,100);
+
+
+
+            g.setColor(Color.magenta);
+            g.fillArc(100,100,100,100,40,180);
+
+            g.setColor(Color.white);
+            g.fillArc(250,275,123,123,180,180);
+            g.setColor(Color.black);
+            g.drawArc(250,275,123,123,180,180);
+
+
+        }
     }
 }
